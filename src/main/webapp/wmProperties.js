f7cf69d6b78ccfbde37ec45db339ea4f106e91a8
QmLbNkUbWQ <automate1@wavemaker.com>
2017-08-13 19:59:39 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_vGNmKBDfrg",
+  "homePage" : "Main",
+  "name" : "Auto_vGNmKBDfrg",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
